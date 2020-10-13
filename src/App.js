58bdeb8a6a6acf930331a1bc633188724ba9aaa8
zustmornah <zustmornah@gmail.com>
2020-10-13 17:15:14 +0000

    development branch

--- src/App.js
+++ src/App.js
@@ -1,54 +1,42 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 //import {useTable} from "react";
-import Login from './Login';
-import Home from './Home';
-
-
-
+import Login from "./Login";
+import Home from "./Home";
 
 function App() {
-  return (
-   <div>
- <Login/> 
- <Home/>
-
-
-  
-   
-     
-      <div className="form-group">
-        <div className="container">
-       <input type="search" className="form-control px-4" placeholder="Enter your city..."/> 
-       </div>
-       </div>
-       <div className="click-me">
-         <div className="search-b">
-      <input type="submit" className="search-weather btn btn-success " value="Search"/> 
-      
-            
-     </div>       
-        
-    
-      </div>
-     
-
-</div>
-  
-  
-  
- 
- 
-
-
-
-  );
-  
- 
-  
+    return ( <
+        div >
+        <
+        Login / >
+        <
+        Home / >
+        <
+        div className = "form-group" >
+        <
+        div className = "container" >
+        <
+        input type = "search"
+        className = "form-control px-4"
+        placeholder = "Enter your city..." /
+        >
+        <
+        /div>{" "} <
+        /div>{" "} <
+        div className = "click-me" >
+        <
+        div className = "search-b" >
+        <
+        input type = "submit"
+        className = "search-weather btn btn-success "
+        value = "Search" /
+        >
+        <
+        /div>{" "} <
+        /div>{" "} <
+        /div>
+    );
 }
 
-  
 export default App;
-
