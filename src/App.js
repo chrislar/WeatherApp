8b7ab2a81e9fee23e44965b1a5f27f324c99335b
chrislar <christianlaryea89@gmail.com>
2020-10-16 08:13:30 +0000

    A commit to my branch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React,{useState} from 'react';
 //import logo from './logo.svg';
 import './App.css';
@@ -34,8 +35,47 @@ const [userlogged, setLogged] = useState(false)
   
  
   
+=======
+import React from "react";
+//import logo from './logo.svg';
+import "./App.css";
+//import {useTable} from "react";
+import Login from "./Login";
+import Home from "./Home";
+
+function App() {
+    return ( <
+        div >
+        <
+        Login / >
+        <
+        Home / >
+        <
+        div className = "form-group" >
+        <
+        div className = "container" >
+        <
+        input type = "search"
+        className = "form-control px-4"
+        placeholder = "Enter your city..." /
+        >
+        <
+        /div>{" "} <
+        /div>{" "} <
+        div className = "click-me" >
+        <
+        div className = "search-b" >
+        <
+        input type = "submit"
+        className = "search-weather btn btn-success "
+        value = "Search" /
+        >
+        <
+        /div>{" "} <
+        /div>{" "} <
+        /div>
+    );
+>>>>>>> d6f1ac6b1b739eeccedf1d3e1089984046722b5d
 }
 
-  
 export default App;
-
